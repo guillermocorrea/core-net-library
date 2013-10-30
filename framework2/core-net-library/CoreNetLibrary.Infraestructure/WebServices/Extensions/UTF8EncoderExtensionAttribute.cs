4e89bab9d8fa3fc49b8f8e219a5888dcf6217167
Guillermo Correa <guille_freelance@hotmail.com>
2013-10-30 17:38:37 -0500

    Added UTF8Enconder attribute

---
+++ framework2/core-net-library/CoreNetLibrary.Infraestructure/WebServices/Extensions/UTF8EncoderExtensionAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Services.Protocols;
+
+namespace CoreNetLibrary.Infraestructure.WebServices.Extensions
+{
+    // Create a SoapExtensionAttribute for the SOAP Extension that can be
+    // applied to a Web service method.
+    [AttributeUsage(AttributeTargets.Method)]
+    public class UTF8EncoderExtensionAttribute : SoapExtensionAttribute
+    {
+        private int priority;
+
+        public override Type ExtensionType
+        {
+            get { return typeof(UTF8EncoderExtension); }
+        }
+
+        public override int Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+    }
+}
